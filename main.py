4bf37dd01e5be85c27afa324935e488e7428a95b
drianbaferlinlo393 <losuicai1995@gmail.com>
2024-12-12 06:40:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ init(autoreset=True)
 def print_banner():
     banner = f"""
 {Fore.RED}{'='*40}
-    {Fore.CYAN}{Style.BRIGHT}CUKIMAY TEAM
+    {Fore.CYAN}{Style.BRIGHT}FATCAT CYBER TEAM Ft JACK007
 {Fore.RED}{'='*40}
 {Fore.YELLOW}{'='*40}
           {Fore.GREEN}Main Tools Menu      
 {Fore.YELLOW}{'='*40}
- {Fore.BLUE}1. Grabber Tools
+ {Fore.BLUE}1. Grabber Premium
  {Fore.BLUE}2. Subfinder Tools
  {Fore.BLUE}3. CMS Scan Tools
  {Fore.BLUE}4. RCE Scan Tools
@@ -22,21 +22,26 @@ def print_banner():
  {Fore.BLUE}9. Convert Hash MD5
  {Fore.BLUE}10. Reverse IP
  {Fore.BLUE}11. Convert Domain To IP
+ {Fore.BLUE}12. Grabber Path Tools
+ {Fore.BLUE}13. 4 In 1
+ {Fore.BLUE}14. Dorking Google
+ {Fore.BLUE}15. WP Username Finder + Brute-forcer
+ {Fore.BLUE}16. XMLRPC
  {Fore.BLUE}0. Exit
 {Fore.YELLOW}{'='*40}
-{Fore.RED}Donasi: 
-{Fore.RED}        https://t.me/xinxhau
+{Fore.RED}Donasi: https://t.me/fatcatcyberteam
+{Fore.RED}        https://t.me/bagudung1995
 {Fore.YELLOW}{'='*40}
     """
     print(banner)
 
 def main():
-    print_banner()
     while True:
+        print_banner()
         choice = input(f"{Fore.WHITE}Pilih Tools : {Fore.MAGENTA}")
 
         if choice == '1':
-            os.system('python files/grabber.py')
+            os.system('python files/grabber3.py')
         elif choice == '2':
             os.system('python files/subfinder.py')
         elif choice == '3':
@@ -57,11 +62,23 @@ def main():
             os.system('python files/rev.py')
         elif choice == '11':
             os.system('python files/domip.py')
+        elif choice == '12':
+            os.system('python files/grabber2.py')
+        elif choice == '13':
+            os.system('python files/4in1.py')
+        elif choice == '14':
+            os.system('python files/dorking.py')
+        elif choice == '15':
+            os.system('python files/WP.py')
+        elif choice == '16':
+            os.system('python files/xmlrpc.py')
         elif choice == '0':
             print(f"{Fore.GREEN}Exiting...")
             break
         else:
             print(f"{Fore.RED}Pilihan tidak valid. Silakan coba lagi.")
+
+        input(f"{Fore.YELLOW}Tekan Enter untuk kembali ke menu...")
 
 if __name__ == "__main__":
     main()
